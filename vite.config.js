fbb72866cd0503fa3f9d1b7cb1b60e81046a0dea
jodyswartz <8488874+jodyswartz@users.noreply.github.com>
2026-06-13 22:22:37 +0000

    LaunchPilot task #105

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
-const packageName = process.env.npm_package_name;
-const projectName = repositoryName ?? packageName;
-const productionBase = process.env.VITE_BASE_PATH ?? (projectName ? `/${projectName}/` : '/');
-
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? productionBase : '/',
   plugins: [react()],
+  base: '/routine-no-1/',
 });
